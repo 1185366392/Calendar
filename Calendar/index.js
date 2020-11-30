40ce4c1dc60c55136d8252bc3c7367152c0d4e43
2.9 <1185366392@qq.com>
2020-11-30 11:36:02 +0800

    实现基本功能

--- Calendar/index.js
+++ Calendar/index.js
@@ -5,7 +5,10 @@
 var tbody=document.getElementById("tbody");
 var date=new Date();
 var year=date.getFullYear();
+var tyear=date.getFullYear();
 var month=date.getMonth()+1;
+var tmonth=date.getMonth()+1;
+//拿来渲染显示今日日期
 var day=date.getDate();
 var toptext=document.getElementById("toptext");
 toptext.innerHTML=year+' - '+month;
@@ -21,6 +24,9 @@ var daynumber=(year,month)=>{
     ]
     return arr[month-1]||(isRunyear(year)?29:28);
 }
+//界面刚加载出来时的渲染
+riqgezi(year,month);
+//点击事件控制页面上内容的显示
 function dj(num){
     if(num==1){
         month++;
@@ -38,14 +44,29 @@ function dj(num){
         }
         toptext.innerHTML=year+' - '+month;
     }
+    riqgezi(year,month);
 }
-for(let i=1;i<=daynumber(year,month);i++){
-    if(((i-1)%7==0&&i!=29)||i==28){
-        var tr=document.createElement("tr");
-        tbody.appendChild(tr);
+//根据月份打印内容
+function riqgezi(year,month){
+    let arrnum=[];
+    for(let i=1;i<=daynumber(year,month);i++){
+        arrnum[i]=i;
     }
-    let rq=document.createElement("td");
-    let text=document.createTextNode(i);
-    rq.appendChild(text);
-    tr.appendChild(rq);
+    console.log(arrnum);
+    let str='';
+    let a=1;
+    for(let i=0;i<5;i++){
+        str+=`<tr>`;
+        for(let j=0;j<7;j++){
+            if(a>daynumber(year,month))
+            break;
+            str+=`<td class='${"a"+a}'>${arrnum[a]}</td>`;
+            a++;
+            if(j===6)
+                str+='</tr>';
+        }
+    }
+    tbody.innerHTML=str;
+    if(year==tyear&&month==tmonth)
+        document.querySelector('.a'+day).className='a today';
 }
